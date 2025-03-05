d3e9e1746c844ca52a27a8f45755c5ea3f08a471
Bob Singor <bob@singor.com>
2025-03-06 01:12:38 +0200

    Create homepage for website

--- website/tailwind.config.ts
+++ website/tailwind.config.ts
@@ -20,26 +20,24 @@ export default {
     letterSpacing: {
       tight: '-0.015em'
     },
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      black: '#000',
-      white: '#fff',
-      gray: {
-        50: '#f9fafb',
-        100: '#f3f4f6',
-        200: '#e5e7eb',
-        300: '#d1d5db',
-        400: '#9ca3af',
-        500: '#6b7280',
-        600: '#4b5563',
-        700: '#374151',
-        800: '#1f2937',
-        900: '#111827'
-      }
-    },
     extend: {
       colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        black: '#000',
+        white: '#fff',
+        gray: {
+          50: '#f9fafb',
+          100: '#f3f4f6',
+          200: '#e5e7eb',
+          300: '#d1d5db',
+          400: '#9ca3af',
+          500: '#6b7280',
+          600: '#4b5563',
+          700: '#374151',
+          800: '#1f2937',
+          900: '#111827'
+        },
         primary: {
           50: '#f4f1f8',
           100: '#e9e4f1',
