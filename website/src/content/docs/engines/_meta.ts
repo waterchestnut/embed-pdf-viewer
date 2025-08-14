9871658be610336eec4b07f483bf9c3089fa0f6f
Bob Singor <bob@singor.com>
2025-08-14 13:37:15 +0300

    Add installation and react

--- website/src/content/docs/engines/_meta.ts
+++ website/src/content/docs/engines/_meta.ts
@@ -1,5 +1,7 @@
 export default {
   introduction: 'Introduction',
+  installation: 'Installation',
+  'react-integration': 'Using with React',
   tasks: 'Tasks',
   'document-lifecycle': 'Document Lifecycle',
   rendering: 'Rendering',
