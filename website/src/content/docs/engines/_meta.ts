64adf6d8a561ffede8829ac3e4e75431e074ad57
Bob Singor <bob@singor.com>
2025-08-14 11:38:04 +0300

    Add annotation models

--- website/src/content/docs/engines/_meta.ts
+++ website/src/content/docs/engines/_meta.ts
@@ -1,4 +1,7 @@
 export default {
   introduction: 'Introduction',
   tasks: 'Tasks',
+  'document-lifecycle': 'Document Lifecycle',
+  rendering: 'Rendering',
+  annotations: 'Annotations',
 }
---
+++ website/src/content/docs/engines/annotations/_meta.ts
@@ -0,0 +1,3 @@
+export default {
+  'annotation-models': 'Annotation Models',
+}
