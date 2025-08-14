e982a94d29194f12cb25c01071f09826a2118708
Bob Singor <bob@singor.com>
2025-08-14 10:22:12 +0300

    Save as copy

--- website/src/content/docs/engines/document-lifecycle/_meta.ts
+++ website/src/content/docs/engines/document-lifecycle/_meta.ts
@@ -1,5 +1,6 @@
 export default {
   'open-document-url': 'Open Document from URL',
   'open-document-buffer': 'Open Document from Buffer',
+  'save-as-copy': 'Save as Copy',
   'close-document': 'Close Document',
 }
