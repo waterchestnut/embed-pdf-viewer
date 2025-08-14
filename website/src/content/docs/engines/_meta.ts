758da05235ebf36dac415ec88796f12e20014acc
Bob Singor <bob@singor.com>
2025-08-14 14:25:10 +0300

    Add vue integration

--- website/src/content/docs/engines/_meta.ts
+++ website/src/content/docs/engines/_meta.ts
@@ -2,6 +2,7 @@ export default {
   introduction: 'Introduction',
   installation: 'Installation',
   'react-integration': 'Using with React',
+  'vue-integration': 'Using with Vue',
   tasks: 'Tasks',
   'document-lifecycle': 'Document Lifecycle',
   rendering: 'Rendering',
