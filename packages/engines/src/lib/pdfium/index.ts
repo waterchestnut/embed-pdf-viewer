55bce0f91af745f6c5eba096927a70fc88c21bfa
Bob Singor <bob@singor.com>
2025-09-01 14:05:06 +0300

    Update the docs! simplify with the use of DEFAULT_PDFIUM_WASM_URL

--- packages/engines/src/lib/pdfium/index.ts
+++ packages/engines/src/lib/pdfium/index.ts
@@ -2,5 +2,5 @@ export * from './engine';
 export * from './helper';
 export * from './runner';
 
-export const DEFAULT_PDFIUM_WASM_URL =
+export const DEFAULT_PDFIUM_WASM_URL: string =
   'https://cdn.jsdelivr.net/npm/@embedpdf/pdfium@__PDFIUM_VERSION__/dist/pdfium.wasm';
