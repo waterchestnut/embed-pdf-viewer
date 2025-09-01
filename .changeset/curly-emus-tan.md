b7410368e5bbe00dca339c9c31b380e913d4e52c
Bob Singor <bob@singor.com>
2025-09-01 13:49:47 +0300

    PDFium URL export

---
+++ .changeset/curly-emus-tan.md
@@ -0,0 +1,5 @@
+---
+'@embedpdf/engines': patch
+---
+
+Export DEFAULT_PDFIUM_WASM_URL for easy way to get the PDFium URL
--- packages/engines/src/lib/pdfium/index.ts
+++ packages/engines/src/lib/pdfium/index.ts
@@ -2,5 +2,5 @@ export * from './engine';
 export * from './helper';
 export * from './runner';
 
-export const DEFAULT_WASM_URL =
+export const DEFAULT_PDFIUM_WASM_URL =
   'https://cdn.jsdelivr.net/npm/@embedpdf/pdfium@__PDFIUM_VERSION__/dist/pdfium.wasm';
