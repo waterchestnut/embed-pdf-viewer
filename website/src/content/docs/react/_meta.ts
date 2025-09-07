a9ddf55c0be29d4ea54ec1a6b900f00129ff87b8
Bob Singor <bob@singor.com>
2025-09-08 00:50:06 +0300

    Add full example to the docs

--- website/src/content/docs/react/_meta.ts
+++ website/src/content/docs/react/_meta.ts
@@ -2,5 +2,6 @@ export default {
   introduction: 'Introduction',
   'getting-started': 'Getting Started',
   'understanding-plugins': 'Understanding Plugins',
+  'full-example': 'Full Example: MUI Viewer',
   engine: 'Engine',
 }
