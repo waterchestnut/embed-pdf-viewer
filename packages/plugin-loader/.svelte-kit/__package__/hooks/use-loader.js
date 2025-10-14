1ca8dea2d4c78c696c92725f620c6da97c885453
Andrew Risse <andrewrisse@defenseunicorns.com>
2025-10-14 10:52:23 -0600

    Remove plugin-loader .sveltekit dir

--- packages/plugin-loader/.svelte-kit/__package__/hooks/use-loader.js
+++
@@ -1,4 +0,0 @@
-import { useCapability, usePlugin } from '@embedpdf/core/svelte';
-import { LoaderPlugin } from '@embedpdf/plugin-loader';
-export const useLoaderPlugin = () => usePlugin(LoaderPlugin.id);
-export const useLoaderCapability = () => useCapability(LoaderPlugin.id);
